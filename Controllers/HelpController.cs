b7ce1beeb40b2559870decc46eeb684eb4008a2a
jeevan1098 <jeevanbabugotru29@gmail.com>
2026-03-02 13:35:56 -0600

    edited alumni page

---
+++ Controllers/HelpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alumni_Management_System.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
